f534a275b6da360827e8602408012b30dba02fdb
shaunfreeman <shaun@shaunfreeman.co.uk>
2011-08-02 17:10:29 +0100

    contacts for client update view started

--- application/modules/power/controllers/ClientController.php
+++ application/modules/power/controllers/ClientController.php
@@ -96,8 +96,8 @@ class Power_ClientController extends BBA_Controller_Action_Abstract
             $this->getForm('clientSave')
                     ->populate($client->toArray('dd/MM/yyyy'))
                     ->addHiddenElement('returnAction', 'edit');
-            
-            $this->view->assign('client', $client);
+
+            $this->view->assign('client', $client->getId());
         } else {
            return $this->_helper->redirector('index', 'client');
         }
