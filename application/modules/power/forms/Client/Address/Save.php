3d0b37a741970c4989bbead617f710340ba1b99e
shaunfreeman <shaun@shaunfreeman.co.uk>
2011-08-02 19:56:03 +0100

    client address save form started

---
+++ application/modules/power/forms/Client/Address/Save.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * AddressSave.php
+ *
+ * Copyright (c) 2011 Shaun Freeman <shaun@shaunfreeman.co.uk>.
+ *
+ * This file is part of BBA.
+ *
+ * BBA is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * BBA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with BBA.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @category   BBA
+ * @package
+ * @subpackage Form
+ * @copyright  Copyright (c) 2011 Shaun Freeman. (http://www.shaunfreeman.co.uk)
+ * @license    http://www.gnu.org/licenses GNU General Public License
+ * @author     Shaun Freeman <shaun@shaunfreeman.co.uk>
+ */
+
+/**
+ * Form Class AddressSave.
+ *
+ * @category   BBA
+ * @package
+ * @subpackage Form
+ * @copyright  Copyright (c) 2011 Shaun Freeman. (http://www.shaunfreeman.co.uk)
+ * @license    http://www.gnu.org/licenses GNU General Public License
+ * @author     Shaun Freeman <shaun@shaunfreeman.co.uk>
+ */
+class Power_Form_Client_Address_Save extends ZendSF_Form_Abstract
+{
+    public function init()
+    {
+        $this->setName('client-address');
+
+        $this->addElement('text', 'clientAd_addressName', array(
+            'label'     => 'Address Name:',
+            'filters'   => array('StripTags', 'StringTrim'),
+            'required'  => true
+        ));
+
+        $this->addElement('text', 'clientAd_address1', array(
+            'label'     => 'Address 1:',
+            'required'  => true,
+            'filters'   => array('StripTags', 'StringTrim')
+        ));
+
+        $this->addElement('text', 'clientAd_address2', array(
+            'label'     => 'Address 2:',
+            'required'  => true,
+            'filters'   => array('StripTags', 'StringTrim')
+        ));
+
+        $this->addElement('text', 'clientAd_address3', array(
+            'label'     => 'Address 3:',
+            'required'  => true,
+            'filters'   => array('StripTags', 'StringTrim')
+        ));
+
+        $this->addElement('text', 'clientAd_postcode', array(
+            'label'     => 'Postcode:',
+            'required'  => true,
+            'filters'   => array('StripTags', 'StringTrim')
+        ));
+
+        $this->addSubmit('Save');
+        $this->addSubmit('Cancel', 'cancel');
+    }
+
+}
