e0eddc8437cf612ffe7a56ca6f26fc566e7a47ef
shaunfreeman <shaun@shaunfreeman.co.uk>
2011-08-08 23:30:56 +0100

    update controllers to fix login timeout problem

--- application/modules/power/controllers/ClientAddressController.php
+++ application/modules/power/controllers/ClientAddressController.php
@@ -49,6 +49,10 @@ class Power_ClientAddressController extends BBA_Controller_Action_Abstract
      */
     public function init()
     {
+        if ($this->_helper->acl('Guest')) {
+            return $this->_forward('login', 'auth');
+        }
+        
         parent::init();
 
         $this->_model = new Power_Model_Mapper_ClientAddress();
