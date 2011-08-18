217d06c2b63b31864fe9763b1230c64d6667980d
shaunfreeman <shaun@shaunfreeman.co.uk>
2011-08-18 18:09:42 +0100

    filtering select done for tender supplier

---
+++ application/modules/power/controllers/SupplierController.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * SupplierController.php
+ *
+ * Copyright (c) 2011 Shaun Freeman <shaun@shaunfreeman.co.uk>.
+ *
+ * This file is part of BBA.
+ *
+ * BBA is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * BBA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with BBA.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @category   BBA
+ * @package    Power
+ * @subpackage Controller
+ * @copyright  Copyright (c) 2011 Shaun Freeman. (http://www.shaunfreeman.co.uk)
+ * @license    http://www.gnu.org/licenses GNU General Public License
+ * @author     Shaun Freeman <shaun@shaunfreeman.co.uk>
+ */
+
+/**
+ * Controller Class SupplierController.
+ *
+ * @category   BBA
+ * @package    Power
+ * @subpackage Controller
+ * @copyright  Copyright (c) 2011 Shaun Freeman. (http://www.shaunfreeman.co.uk)
+ * @license    http://www.gnu.org/licenses GNU General Public License
+ * @author     Shaun Freeman <shaun@shaunfreeman.co.uk>
+ */
+class Power_SupplierController extends ZendSF_Controller_Action_Abstract {
+
+    /**
+     * Initialization code.
+     */
+    public function init()
+    {
+        parent::init();
+
+        $this->_model = new Power_Model_Mapper_Supplier();
+    }
+
+    /**
+     * Default action
+     */
+    public function indexAction()
+    {
+        // action body
+    }
+
+    public function autocompleteAction()
+    {
+        $this->_helper->layout->disableLayout();
+        $this->getHelper('viewRenderer')->setNoRender(true);
+
+        switch ($this->_request->getParam('param')) {
+            case 'supplier':
+                $identifier = 'supplier_idSupplier';
+                $searchItems = array('idSupplier', 'name');
+                $result = $this->_model->fetchAll();
+                break;
+        }
+
+        $items = array();
+
+        foreach ($result as $row) {
+            //$row = $row->toArray();
+            $items[] = array(
+                $identifier                     => $row->{$searchItems[0]},
+                $row->prefix . $searchItems[1]  => $row->{$searchItems[1]}
+            );
+        }
+
+        $data = new Zend_Dojo_Data($identifier, $items);
+        echo $data->toJson();
+
+
+    }
+
+}
--- application/modules/power/forms/Tender/Save.php
+++ application/modules/power/forms/Tender/Save.php
@@ -41,6 +41,19 @@ class Power_Form_Tender_Save extends ZendSF_Form_Abstract
 {
     public function init()
     {
+        $this->addElement('FilteringSelect', 'tender_idSupplier', array(
+            'label'         => 'Supplier:',
+            'filters'       => array('StripTags', 'StringTrim'),
+            'autoComplete'  => false,
+            'hasDownArrow'  => true,
+            'storeId'       => 'supplierStore',
+            'storeType'     => 'dojo.data.ItemFileReadStore',
+            'storeParams'   => array('url' => "/supplier/autocomplete/param/supplier"),
+            'dijitParams'   => array('searchAttr' => 'supplier_name'),
+            //'attribs'       => array('readonly' => true),
+            'required'      => true
+        ));
+
         $this->addElement('NumberSpinner', 'tender_periodContract', array(
             'label'     => 'Tender Period:',
             'min'       => 0,
