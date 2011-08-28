407efd4e0b7cfbcf41567d8b408942ef684d0922
shaunfreeman <shaun@shaunfreeman.co.uk>
2011-08-28 22:26:57 +0100

    turned mapper cache off for now

--- application/modules/power/controllers/ContractController.php
+++ application/modules/power/controllers/ContractController.php
@@ -81,7 +81,7 @@ class Power_ContractController extends BBA_Controller_Action_Abstract
             'meter'    => $this->_request->getParam('meter')
         );
 
-        $contracts = $this->_model->getCached('contracts')->contractSearch($search);
+        $contracts = $this->_model->contractSearch($search);
 
         $dataStore = $this->getDataStore($contracts, 'contract_idContract');
 
