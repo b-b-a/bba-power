7e7c5c5e793d4771f327e08d6288a007b7dcb6de
shaunfreeman <shaun@shaunfreeman.co.uk>
2011-08-31 20:21:07 +0100

    supplier forms started

---
+++ application/modules/power/forms/Supplier/Search.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Search.php
+ *
+ * Copyright (c) 2011 Shaun Freeman <shaun@shaunfreeman.co.uk>.
+ *
+ * This file is part of BBA.
+ *
+ * BBA is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * BBA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with BBA.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @category   BBA
+ * @package    Power
+ * @subpackage Form_Supplier
+ * @copyright  Copyright (c) 2011 Shaun Freeman. (http://www.shaunfreeman.co.uk)
+ * @license    http://www.gnu.org/licenses GNU General Public License
+ * @author     Shaun Freeman <shaun@shaunfreeman.co.uk>
+ */
+
+/**
+ * Form Class Search.
+ *
+ * @category   BBA
+ * @package    Power
+ * @subpackage Form_Supplier
+ * @copyright  Copyright (c) 2011 Shaun Freeman. (http://www.shaunfreeman.co.uk)
+ * @license    http://www.gnu.org/licenses GNU General Public License
+ * @author     Shaun Freeman <shaun@shaunfreeman.co.uk>
+ */
+class Power_Form_Supplier_Search extends Power_Form_SearchBase
+{
+    public function init()
+    {
+        $this->addElement('TextBox', 'supplier', array(
+            'label'     => 'Supplier:',
+            'attribs'   => array('class' => 'search'),
+            'filters'   => array('StripTags', 'StringTrim')
+        ));
+
+        $this->addElement('TextBox', 'contact', array(
+            'label'     => 'Contact:',
+            'attribs'   => array('class' => 'search'),
+            'filters'   => array('StripTags', 'StringTrim')
+        ));
+        
+        parent::init();
+    }
+
+}
