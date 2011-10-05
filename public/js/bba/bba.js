d6954482809cb12ae05913ff41fe679785562f45
shaunfreeman <shaun@shaunfreeman.co.uk>
2011-10-05 20:46:22 +0100

    #16 this might be a bit of a hack, but it seems to be OK for now

--- public/js/bba/bba.js
+++ public/js/bba/bba.js
@@ -12,8 +12,23 @@ dojo.declare("bba", null, {
         this.grids = dijit.registry.byClass("dojox.grid.DataGrid");
 
         this.grids.forEach(function(grid) {
-            this.gridRowClick(grid);
+            dojo.connect(grid, "onRowClick", this, function(val) {
+                this.gridRowClick(grid);
+            });
         }, this);
+        
+        if (!location.href.match('edit') && !location.href.match('add')) {
+            setTimeout(function(){
+                this.gridLineCheck();
+            }.bind(this), 250);
+        }
+    },
+    
+    gridLineCheck : function() {
+        grid = this.grids.toArray()[0];
+        if(grid.rowCount == 1) {
+            this.gridRowClick(grid, 0);
+        }
     },
 
     gridLink : function(selectedId, inputName, url) {
@@ -31,9 +46,11 @@ dojo.declare("bba", null, {
         formNode.submit();
     },
 
-    gridRowClick : function(grid) {
-        dojo.connect(grid, "onRowClick", this, function(val) {
-            selectedIndex = grid.focus.rowIndex;
+    gridRowClick : function(grid, selectedIndex) {
+            if (selectedIndex == null) {
+                selectedIndex = grid.focus.rowIndex;
+            }
+            
             selectedItem = grid.getItem(selectedIndex);
 
             ident = grid.store._identifier;
@@ -47,7 +64,6 @@ dojo.declare("bba", null, {
             selectedId = grid.store.getValue(selectedItem, ident);
 
             this.gridLink(selectedId, i[1], "/" + this.hyphenate(i[0]) + "/edit");
-        });
     },
 
     hyphenate: function(str) {
