a63f702fceffba67349589e8c42950518bdcf5cf
shaunfreeman <shaun@shaunfreeman.co.uk>
2011-11-25 01:02:30 +0000

    changes to meter search and added some styling to contract display.

--- application/modules/power/models/DbTable/Meter.php
+++ application/modules/power/models/DbTable/Meter.php
@@ -111,16 +111,13 @@ class Power_Model_DbTable_Meter extends Zend_Db_Table_Abstract
             ->join('client', 'client_idClient = site_idClient', array(
                 'client_name'
             ))
-            ->join('meter_contract', 'meter_idMeter = meterContract_idMeter', null)
-            ->join('contract', 'meterContract_idContract = contract_idContract', array(
+            ->joinLeft('meter_contract', 'meter_idMeter = meterContract_idMeter', null)
+            ->joinLeft('contract', 'meterContract_idContract = contract_idContract', array(
                 'contract_status',
-                //'MAX(contract.contract_dateStart)',
+                'contract_dateStart' => 'MAX(contract.contract_dateStart)',
                 'contract_dateEnd'
             ))
-            //->group('meter_idMeter')
-            ->where('CURRENT_DATE BETWEEN contract_dateStart AND contract_dateEnd')
-       //     ->where('contract_status IN (?)', array('current', 'signed', 'selected', 'choose'))
-           ;
+            ->group('meter_idMeter');
     }
 
     public function getSearch($search, $select)
