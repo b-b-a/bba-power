019fd6b845df66fb6b6226abdc3d240ed45e4702
shaunfreeman <shaun@shaunfreeman.co.uk>
2012-01-18 13:07:51 +0000

    removed date->set as not really needed

--- application/modules/power/models/Contract.php
+++ application/modules/power/models/Contract.php
@@ -236,8 +236,6 @@ class Power_Model_Contract extends ZendSF_Model_Acl_Abstract
         foreach ($data as $key => $value) {
             if (in_array($key, $dateKeys) && $value != '') {
                 $date = new Zend_Date($value, Zend_Date::DATE_SHORT);
-
-                $date->set($date->toString('yy'), Zend_Date::YEAR_SHORT);
                 $data[$key] = $date->toString('yyyy-MM-dd');
             }
         }
@@ -318,8 +316,6 @@ class Power_Model_Contract extends ZendSF_Model_Acl_Abstract
         foreach ($data as $key => $value) {
             if (in_array($key, $dateKeys)) {
                 $date = new Zend_Date($value, Zend_Date::DATE_SHORT);
-
-                $date->set($date->toString('yy'), Zend_Date::YEAR_SHORT);
                 $data[$key] = $date->toString('yyyy-MM-dd');
             }
         }
