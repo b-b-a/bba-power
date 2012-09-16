18aff14d44c16c1f6daf8ced69c235d3557cf7ce
shaunfreeman <shaun@shaunfreeman.co.uk>
2012-09-16 22:11:06 +0100

    hopefully last dojo missing file for production build

--- public/js/bba/builder/CompileAllJs.sh
+++ public/js/bba/builder/CompileAllJs.sh
@@ -38,21 +38,23 @@ cp -vr bba-temp/dojox/grid/resources/images/ bba/dojox/grid/resources
 mkdir -v --parents bba/dijit/icons/
 mkdir -v --parents bba/dijit/themes/claro/
 mkdir -v --parents bba/dijit/nls/
+mkdir -v --parents bba/dijit/form/nls/
 
+cp -vr bba-temp/dijit/form/nls/validate.js bba/dijit/form/nls/validate.js
 cp -vr bba-temp/dijit/nls/loading.js bba/dijit/nls/loading.js
 cp -vr bba-temp/dijit/icons/ bba/dijit
 cp -vr bba-temp/dijit/themes/claro/ bba/dijit/themes
 cp -vr bba-temp/dijit/themes/dijit.css bba/dijit/themes/dijit.css
 cp -vr bba-temp/dijit/themes/dijit_rtl.css bba/dijit/themes/dijit_rtl.css
 
-mkdir -v --parents bba/dojo/cldr/nls/en/
-mkdir -v --parents bba/dojo/cldr/nls/en-gb/
+#mkdir -v --parents bba/dojo/cldr/nls/en/
+#mkdir -v --parents bba/dojo/cldr/nls/en-gb/
 mkdir -v --parents bba/dojo/resources/
 mkdir -v --parents bba/dojo/nls/
 
 cp -vr bba-temp/dojo/nls/dojo_en-gb.js bba/dojo/nls/dojo_en-gb.js
-cp -vr bba-temp/dojo/cldr/nls/en/ bba/dojo/cldr/nls
-cp -vr bba-temp/dojo/cldr/nls/en-gb/ bba/dojo/cldr/nls
+#cp -vr bba-temp/dojo/cldr/nls/en/ bba/dojo/cldr/nls
+#cp -vr bba-temp/dojo/cldr/nls/en-gb/ bba/dojo/cldr/nls
 cp -vr bba-temp/dojo/resources/ bba/dojo
 rm -v bba/dojo/resources/_modules.js
 cp -vr bba-temp/dojo/dojo.js bba/dojo/dojo.js
