a5ce18b171a47ff0329be0c0a2d846916d21f224
shaunfreeman <shaun@shaunfreeman.co.uk>
2012-09-19 19:46:27 +0100

    missing cldr dirs put back

--- public/js/bba/builder/CompileAllJs.sh
+++ public/js/bba/builder/CompileAllJs.sh
@@ -47,14 +47,14 @@ cp -vr bba-temp/dijit/themes/claro/ bba/dijit/themes
 cp -vr bba-temp/dijit/themes/dijit.css bba/dijit/themes/dijit.css
 cp -vr bba-temp/dijit/themes/dijit_rtl.css bba/dijit/themes/dijit_rtl.css
 
-#mkdir -v --parents bba/dojo/cldr/nls/en/
-#mkdir -v --parents bba/dojo/cldr/nls/en-gb/
+mkdir -v --parents bba/dojo/cldr/nls/en/
+mkdir -v --parents bba/dojo/cldr/nls/en-gb/
 mkdir -v --parents bba/dojo/resources/
 mkdir -v --parents bba/dojo/nls/
 
 cp -vr bba-temp/dojo/nls/dojo_en-gb.js bba/dojo/nls/dojo_en-gb.js
-#cp -vr bba-temp/dojo/cldr/nls/en/ bba/dojo/cldr/nls
-#cp -vr bba-temp/dojo/cldr/nls/en-gb/ bba/dojo/cldr/nls
+cp -vr bba-temp/dojo/cldr/nls/en/ bba/dojo/cldr/nls
+cp -vr bba-temp/dojo/cldr/nls/en-gb/ bba/dojo/cldr/nls
 cp -vr bba-temp/dojo/resources/ bba/dojo
 rm -v bba/dojo/resources/_modules.js
 cp -vr bba-temp/dojo/dojo.js bba/dojo/dojo.js
