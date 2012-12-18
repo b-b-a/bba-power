ffd9cc970cd80b4ed7a106d369e0f798e4a5f00b
Eddie Pounce <eddie-github@p-cs.co.uk>
2012-12-18 15:45:16 +0000

    4.0.2 loaded into BBA Production & PreProd 18/12/2012 17:00 ish

--- application/modules/power/services/Version.php
+++ application/modules/power/services/Version.php
@@ -47,7 +47,7 @@ final class Power_Service_Version
 	 */
 	public static function getVersion()
 	{
-		return '4.0.2c' . self::getEnvironment();
+		return '4.0.2' . self::getEnvironment();
 	}
 
     /**
