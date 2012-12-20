1515e6e072707030e3fcd31571536c8ed45f49ae
Eddie Pounce <eddie-github@p-cs.co.uk>
2012-12-20 17:49:09 +0000

    Provide a way of seeing AddressID being used for a Site in GUI

--- application/modules/power/services/Version.php
+++ application/modules/power/services/Version.php
@@ -47,7 +47,7 @@ final class Power_Service_Version
 	 */
 	public static function getVersion()
 	{
-		return '4.0.3a' . self::getEnvironment();
+		return '4.0.3b' . self::getEnvironment();
 	}
 
     /**
