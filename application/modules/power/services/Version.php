6f026c76e0b38e9236f52e0522e91d885635e747
Eddie Pounce <eddie-github@p-cs.co.uk>
2013-03-11 13:52:06 +0000

    4.0.3-  Made Live

--- application/modules/power/services/Version.php
+++ application/modules/power/services/Version.php
@@ -47,7 +47,7 @@ final class Power_Service_Version
 	 */
 	public static function getVersion()
 	{
-		return '4.0.3e' . self::getEnvironment();
+		return '4.0.3' . self::getEnvironment();
 	}
 
     /**
