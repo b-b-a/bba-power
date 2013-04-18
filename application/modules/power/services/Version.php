267c4082231948af23c9f76a924bd796f34b7487
Eddie Pounce <eddie-github@p-cs.co.uk>
2013-04-18 17:35:32 +0100

    Loaded into pre-prod 17:00 18/4/13

--- application/modules/power/services/Version.php
+++ application/modules/power/services/Version.php
@@ -47,7 +47,7 @@ final class Power_Service_Version
 	 */
 	public static function getVersion()
 	{
-		return '4.0.3' . self::getEnvironment();
+		return '4.0.4a' . self::getEnvironment();
 	}
 
     /**
